381456168b851cbbe5c690aef736595c823d6c75
unknown <guptat320@gmail.com>
2021-09-18 16:36:02 +0530

    added text area

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import img from "./Components/black7.jpg";
-import Text from "./Components/Text";
+import React, { useState, useEffect, useRef } from "react";
+import img from "./Components/a.jpg";
+import randomwords from "random-words";
 
 function App() {
   const time = 30;
+  const number_of_words = 50;
   const [countdown, setCountdown] = useState(time);
-  const [currInput, setCurrInput] = useState([]);
-  function start() {
-    let interval = setInterval(() => {
-      setCountdown((prev) => {
-        if (prev === 0) {
-          // clearInterval(interval);
-          return prev;
-        } else {
-          return prev - 1;
-        }
-      });
-    }, 1000);
+  const [currInput, setCurrInput] = useState("");
+  const [words, setWords] = useState([]);
+  const [charindex, setCharindex] = useState(-1);
+  const [charatindex, setCharatindex] = useState("");
+  const [currwordindex, setCurrentwordindex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [status, setStatus] = useState("waiting");
+  const textinput = useRef(null);
+
+  useEffect(() => {
+    if (status === "started") {
+      textinput.current.focus();
+    }
+  }, [status]);
+
+  useEffect(() => {
+    setWords(generatewords());
+  }, []);
+
+  function generatewords() {
+    return new Array(number_of_words).fill(null).map(() => randomwords());
   }
+  function start() {
+    if (status === "finished") {
+      setWords(generatewords());
+      setCorrect(0);
+      setIncorrect(0);
+      setCurrentwordindex(0);
+      setCharindex(-1);
+      setCharatindex("");
+    }
+    if (status !== "started") {
+      setStatus("started");
+      let interval = setInterval(() => {
+        setCountdown((prev) => {
+          if (prev === 0) {
+            clearInterval(interval);
+            setStatus("finished");
+            setCurrInput("");
+            return time;
+          } else {
+            return prev - 1;
+          }
+        });
+      }, 1000);
+    }
+  }
+
+  function handleKeydown({ keyCode, key }) {
+    if (keyCode === 32) {
+      checkmatch();
+      setCurrInput("");
+      setCurrentwordindex(currwordindex + 1);
+      setCharindex(-1);
+    } else if (keyCode === 8) {
+      setCharindex(charindex - 1);
+      setIncorrect(incorrect + 1);
+      setCharatindex("");
+    } else {
+      setCharindex(charindex + 1);
+      setCharatindex(key);
+    }
+  }
+
+  function checkmatch() {
+    const wordtocompare = words[currwordindex];
+    const doesmatch = wordtocompare === currInput.trim();
+    console.log(doesmatch);
+    if (doesmatch) {
+      setCorrect(correct + 1);
+    } else {
+      setIncorrect(incorrect + 1);
+    }
+  }
+
+  function getColor(i, j, ch) {
+    if (
+      currwordindex === i &&
+      charindex === j &&
+      charatindex &&
+      status !== "finished"
+    ) {
+      if (charatindex === ch) {
+        return "has-background-success";
+      } else {
+        return "has-background-danger";
+      }
+    } else if (
+      currwordindex === i &&
+      charindex >= words[currwordindex].length
+    ) {
+      return "has-background-danger";
+    } else {
+      return "";
+    }
+  }
+
   return (
     <div
       style={{
@@ -35,20 +121,65 @@ function App() {
       >
         Speedy
       </h1>
-      <Text currInput={currInput} />
-      <h2 style={{ color: "gold", marginTop: "3vh" }}>{countdown}</h2>
+      <div className="mt-1 " style={{ color: "white" }}>
+        {words.map((word, i) => (
+          <span>
+            <span key={i}>
+              {word.split("").map((ch, j) => (
+                <span
+                  className={getColor(i, j, ch)}
+                  key={j}
+                  style={{ fontSize: "4vh" }}
+                >
+                  {ch}
+                </span>
+              ))}
+            </span>
+            <span> </span>
+          </span>
+        ))}
+      </div>
+      <h2 style={{ color: "gold", marginTop: "3vh", marginLeft: "2vh" }}>
+        {countdown}
+      </h2>
       <textarea
-        className="form-control mb-3 mt-3"
+        ref={textinput}
+        disabled={status !== "started"}
+        className="form-control mb-2 mt-3"
         placeholder="Press Start and Start typing..."
+        onKeyDown={handleKeydown}
+        value={currInput}
         onChange={(e) => {
-          const str=e.target.value;
-          setCurrInput([...currInput,str.charAt(str.length-1)]);
+          setCurrInput(e.target.value);
         }}
-        rows={4}
+        rows={3}
       />
-      <button className="ml-5 mt-4" style={{ fontSize: "4vh" }} onClick={start}>
+      <button
+        className="mt-2"
+        style={{
+          fontSize: "4vh",
+          marginLeft: "100vh",
+        }}
+        onClick={start}
+      >
         START
       </button>
+      {status === "finished" && (
+        <div className="columns" style={{ color: "grey" }}>
+          <div className="column has-text-centered">
+            <p className="is-size-5">Words Per Minutes :</p>
+            <p className="is-size-1" style={{ color: "gold" }}>
+              {2 * correct}
+            </p>
+          </div>
+          <div className="column has-text-centered">
+            <p className="is-size-5">Accuracy :</p>
+            <p className="is-size-1" style={{ color: "gold" }}>
+              {Math.round((correct / (correct + incorrect)) * 100)}%
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
